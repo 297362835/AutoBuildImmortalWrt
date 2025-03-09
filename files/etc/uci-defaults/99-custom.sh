c681e21a979341575548739c4dd22acac0f1f2a0
unclepang <51317230+297362835@users.noreply.github.com>
2025-03-09 14:01:05 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -79,8 +79,8 @@ elif [ "$count" -gt 1 ]; then
       echo "PPPoE is enabled at $(date)" >> $LOGFILE
       # 设置ipv4宽带拨号信息
       uci set network.wan.proto='pppoe'
-      uci set network.wan.username='990003108487'
-      uci set network.wan.password='665684'
+      uci set network.wan.username=$pppoe_account
+      uci set network.wan.password=$pppoe_password
       uci set network.wan.peerdns='1'
       uci set network.wan.auto='1'
       # 设置ipv6 默认不配置协议
